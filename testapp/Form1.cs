3fe03460d7592da2c3a733723550e02c4cf980d5
Nassime Belhad <1870874@cmontmorency.qc.ca>
2022-09-21 11:20:11 -0400

    test 1

--- testapp/Form1.cs
+++ testapp/Form1.cs
@@ -19,14 +19,7 @@ namespace testapp
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            if (textBox1.Text != "")
-            {
-                button1.Enabled = false;
-            }
-            else
-            {
-                button1.Enabled = true;
-            }
+            
         }
     }
 }
